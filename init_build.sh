3b59757ab01f41093de0189dc28d6a202d7d2c80
lentychang <lixis630@gmail.com>
2019-05-06 07:09:06 +0000

    fix typo in image name for miniconda

--- init_build.sh
+++ init_build.sh
@@ -9,7 +9,7 @@ docker build -t gazebo:latest -f gazebo/Docker/Dockerfile.latest .
 docker build --build-arg ENABLE_NVIDIA=$enable_nvidia -t moveit:base -f moveit/Docker/Dockerfile.base . 
 docker build -t moveit:ide -f moveit/Docker/Dockerfile.ide . 
 docker build -t moveit:latest -f moveit/Docker/Dockerfile.latest . 
-docker build -t conda:python3.7 -f conda/Dockerfile.python3 .
+docker build -t miniconda:python3.7 -f conda/Dockerfile.python3 .
 docker build --build-arg ENABLE_NVIDIA=$enable_nvidia -t pythonocc:base -f pythonocc/Docker/Dockerfile.base . 
 docker build -t pythonocc:ide -f pythonocc/Docker/Dockerfile.ide . 
 docker build -t pythonocc:latest -f pythonocc/Docker/Dockerfile.latest . 
